a992ee623ec47d623ab0b88b7fd9b5647e5bc8dd
Gyapi <jozsef.daniel.vekas@gmail.com>
2018-04-05 13:12:53 +0200

    BackendConnection

---
+++ src/app/calendar/day/day.component.ts
@@ -0,0 +1,64 @@
+import {Component, Input, OnInit } from '@angular/core';
+import { DayService } from './day.service'
+
+@Component({
+  selector: 'app-day',
+  templateUrl: './day.component.html',
+  styleUrls: ['./day.component.css']
+})
+export class DayComponent implements OnInit {
+
+  @Input() day: number;
+  @Input() month: number;
+  @Input() year: number;
+  name : string;
+  buttonLabel: string = "Activate";
+  sumString: string = "Work Sum";
+  extraString: string = "Extra Work";
+  newOrNot: boolean;
+
+  constructor(private dayServer: DayService) {
+  }
+
+  ngOnInit() {
+    if (this.day < 1 ){
+      this.name = "";
+      this.buttonLabel = "";
+      this.sumString = "";
+      this.extraString = "";
+    } else {
+      this.name = this.day + "";
+    }
+  }
+
+  ngAfterViewInit(){
+    if (this.day < 1 ){
+      document.getElementById('container' + this.day).classList.add("disabled");
+      document.getElementById('inReq' + this.day).setAttribute('disabled', 'disabled');
+      document.getElementById('btn' + this.day).setAttribute('disabled', 'disabled');
+    } else {
+      this.getWorkDayData();
+    }
+  }
+
+  getWorkDayData(){
+
+    let gaveMonth = this.month + 1;
+
+    this.dayServer.getDay(this.year, gaveMonth, this.day).subscribe(
+      dayData => {
+        document.getElementById('inReq' + this.day).setAttribute('value', dayData.Required);
+        console.log(dayData);
+      },
+    error => {
+      this.newOrNot = false;
+    })
+
+  }
+
+
+  dayBtnEvent(){
+    console.log("anyád");
+  }
+
+}
---
+++ src/app/calendar/day/day.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs/Observable";
+import { HttpHeaders } from '@angular/common/http';
+
+@Injectable()
+export class DayService {
+
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+
+  urlString : string = 'http://localhost:8080/timelogger/workmonths/';
+
+
+  public getDay(year: number, month: number, day: number) : Observable<any> {
+
+    let url = this.urlString + year + '/' + month + '/' + day;
+    return this.http.get(url);
+  }
+
+  public addWorkDay(newDay : JSON){
+    return this.http.post('http://localhost:8080/timelogger/workmonths/workdays', newDay, this.httpOptions);
+  }
+
+}
